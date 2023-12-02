38964b0a8992b8ae381866140db84c5485085256
ja153903 <jaimeabbariao@gmail.com>
2023-12-02 12:37:45 -0500

    feat: updating name from ball to cube

--- yr2023/day2/solve.py
+++ yr2023/day2/solve.py
@@ -5,33 +5,33 @@ from typing import DefaultDict, TypedDict
 from utils.base_solution import BaseSolution
 
 
-class Ball(TypedDict):
+class Cube(TypedDict):
     red: int
     blue: int
     green: int
 
 
-def default_ball_state() -> Ball:
+def default_cube_state() -> Cube:
     return {"red": 0, "blue": 0, "green": 0}
 
 
 class Solution(BaseSolution):
     def part1(self) -> int:
-        balls_by_game_id = self.get_balls_by_game_id()
+        cubes_by_game_id = self.get_cubes_by_game_id()
 
         return sum(
             int(key)
-            for key, record in balls_by_game_id.items()
+            for key, record in cubes_by_game_id.items()
             if record["red"] <= 12 and record["blue"] <= 14 and record["green"] <= 13
         )
 
     def part2(self) -> int:
-        balls_by_game_id = self.get_balls_by_game_id()
+        cubes_by_game_id = self.get_cubes_by_game_id()
 
-        return sum(math.prod(record.values()) for record in balls_by_game_id.values())
+        return sum(math.prod(record.values()) for record in cubes_by_game_id.values())
 
-    def get_balls_by_game_id(self) -> DefaultDict[str, Ball]:
-        balls_by_id = defaultdict(default_ball_state)
+    def get_cubes_by_game_id(self) -> DefaultDict[str, Cube]:
+        cubes_by_id = defaultdict(default_cube_state)
 
         for line in self.data:
             left, right = line.split(": ")
@@ -42,8 +42,8 @@ class Solution(BaseSolution):
                 color_counts = game_set.split(", ")
                 for color_count in color_counts:
                     count, color = color_count.split(" ")
-                    balls_by_id[game_id][color] = max(
-                        balls_by_id[game_id][color], int(count)
+                    cubes_by_id[game_id][color] = max(
+                        cubes_by_id[game_id][color], int(count)
                     )
 
-        return balls_by_id
+        return cubes_by_id
