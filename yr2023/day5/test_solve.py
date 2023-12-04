68a9ab016375aa305d609b938829536e2f801f87
ja153903 <jaimeabbariao@gmail.com>
2023-12-04 15:22:27 -0500

    feat: setup for day 5

---
+++ yr2023/day5/test_solve.py
@@ -0,0 +1,24 @@
+from yr2023.day5.solve import Solution
+
+YEAR = 2023
+DAY = 5
+
+
+example_solution = Solution(YEAR, DAY, is_test=True)
+solution = Solution(YEAR, DAY)
+
+
+def test_aoc2023_day5_part1_example():
+    assert example_solution.part1() == 0
+
+
+def test_aoc2023_day5_part1():
+    assert solution.part1() == 0
+
+
+def test_aoc2023_day5_part2_example():
+    assert example_solution.part2() == 0
+
+
+def test_aoc2023_day5_part2():
+    assert solution.part2() == 0
