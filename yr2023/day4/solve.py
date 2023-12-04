52bb288a1db08e4345d0e9f661a0b58c5e8deb7f
ja153903 <jaimeabbariao@gmail.com>
2023-12-04 00:41:14 -0500

    feat: solving day 4

--- yr2023/day4/solve.py
+++ yr2023/day4/solve.py
@@ -1,9 +1,67 @@
+import re
+from collections import Counter
+from typing import TypedDict
+
 from utils.base_solution import BaseSolution
+
+
+class Entry(TypedDict):
+    id: str
+    winning_numbers: list[int]
+    owned_numbers: list[int]
 
 
 class Solution(BaseSolution):
     def part1(self):
-        ...
+        return sum(
+            self.get_points(self.get_entry_from_line(line)) for line in self.data
+        )
 
     def part2(self):
-        ...
+        freq = Counter()
+        for line in self.data:
+            entry = self.get_entry_from_line(line)
+            freq[entry["id"]] += 1
+
+            entry_id_as_int = int(entry["id"])
+            num_matches = self.get_num_matches(entry)
+
+            for i in range(1, num_matches + 1):
+                freq[str(entry_id_as_int + i)] += freq[entry["id"]]
+
+        return sum(freq.values())
+
+    def get_entry_from_line(self, line: str) -> Entry:
+        left, right = line.split(": ")
+        *_, id = re.split(r"\s+", left)
+        winning, owned = right.split(" | ")
+
+        winning_numbers = list(map(int, re.split("\s+", winning.strip())))
+        owned_numbers = list(map(int, re.split("\s+", owned.strip())))
+
+        return {
+            "id": id,
+            "winning_numbers": winning_numbers,
+            "owned_numbers": owned_numbers,
+        }
+
+    def get_points(self, entry: Entry) -> int:
+        matches = 0
+
+        for num in entry["winning_numbers"]:
+            if num in entry["owned_numbers"]:
+                matches += 1
+
+        if matches == 0:
+            return 0
+
+        return 2 ** (matches - 1)
+
+    def get_num_matches(self, entry: Entry) -> int:
+        matches = 0
+
+        for num in entry["winning_numbers"]:
+            if num in entry["owned_numbers"]:
+                matches += 1
+
+        return matches
--- yr2023/day4/test_solve.py
+++ yr2023/day4/test_solve.py
@@ -8,16 +8,16 @@ solution = Solution(YEAR, DAY)
 
 
 def test_aoc2023_day4_part1_example():
-    assert example_solution.part1() == 0
+    assert example_solution.part1() == 13
 
 
 def test_aoc2023_day4_part1():
-    assert solution.part1() == 0
+    assert solution.part1() == 21959
 
 
 def test_aoc2023_day4_part2_example():
-    assert example_solution.part2() == 0
+    assert example_solution.part2() == 30
 
 
 def test_aoc2023_day4_part2():
-    assert solution.part2() == 0
+    assert solution.part2() == 5132675
