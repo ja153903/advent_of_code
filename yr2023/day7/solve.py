bf5c3293eba848f48bbe6fe0e4505f879d2705fa
ja153903 <jaimeabbariao@gmail.com>
2023-12-06 14:46:44 -0500

    feat: setting up for day 7

---
+++ yr2023/day7/solve.py
@@ -0,0 +1,9 @@
+from utils.base_solution import BaseSolution
+
+
+class Solution(BaseSolution):
+    def part1(self):
+        return super().part1()
+
+    def part2(self):
+        return super().part2()
---
+++ yr2023/day7/test_solve.py
@@ -0,0 +1,23 @@
+from yr2023.day7.solve import Solution
+
+YEAR = 2023
+DAY = 7
+
+example_solution = Solution(YEAR, DAY, is_test=True)
+solution = Solution(YEAR, DAY)
+
+
+def test_aoc2023_day7_part1_example():
+    assert example_solution.part1() == 0
+
+
+def test_aoc2023_day7_part1_prod():
+    assert solution.part1() == 0
+
+
+def test_aoc2023_day7_part2_example():
+    assert example_solution.part2() == 0
+
+
+def test_aoc2023_day7_part2_prod():
+    assert solution.part2() == 0
